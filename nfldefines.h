a61ef4db7e8666d5e5001b9d78bd07ff77561023
Marc MERLIN <marc_soft@merlins.org>
2019-03-20 22:30:13 -0700

    Added support for 3 kinds of panels heights: 32, 64, 96.
    Define mheight that can be used at compile time
    Added demos list for 96 height
    Shrunk number of demos for 96 height
    Used ARRAY_SIZE

--- nfldefines.h
+++ nfldefines.h
@@ -5,27 +5,55 @@
 //#define NEOMATRIX
 #include "neomatrix_config.h"
 
+#ifdef ESP8266
+    #define mheight 32
+#elif defined(ESP32)
+    #ifdef NEOMATRIX
+	#define mheight 96
+    #else
+	#define mheight 64
+    #endif
+#else
+#error "Matrix config undefined, please set height"
+#endif
+
 // On ESP32, I have a 64x64 direct matrix (not tiled) with 2 options of drivers.
-#ifdef ESP32
+#if mheight == 96
+    // Using RGBPanel via SmartMatrix
+
+    #define LAST_MATRIX 87
+    // Which demos are shown, and in which order
+    // 25 pacman, 12 smiley face, fix 28, 31, 35 wave looks bad, 37 also looks bad, 39 not big enough
+    // 32 also looks bad on mismatched size, 9 is BM, 46 not balanced
+    uint8_t demo_mapping[] = { LAST_MATRIX, 21, 2, 23, 18, 19, 22, 20, 24, 3, 26, 27, 29, 30, 33, 34, 36, 40, 41, 42, 43, 44, 45, 7, 47, 48, 49, 50, 8, 51, 52, 53, 54, 55, 56, };
+    // 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+    uint8_t best_mapping[] = { 9, 25, 27, 55, 66, LAST_MATRIX };
+
+    #define RECV_PIN 34
+
+    #define STRIP_NUM_LEDS 48
+    CRGB leds[STRIP_NUM_LEDS];
+    //#define NEOPIXEL_PIN 13
+#elif mheight == 64
     // 64x64 matrix with optional 16 pin parallel driver
     // 55fps without 16PINS, 110fps with 16PINS
     #define ESP32_16PINS
 
     #define LAST_MATRIX 87
     // Which demos are shown, and in which order
-    // 24 buggy storm, 25 pacman, 57 BM gif
-    uint8_t demo_mapping[] = { 57, LAST_MATRIX, 2, 23, 1, 18, 19, 20, 21, 22, 23, 3, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 7, 46, 47, 48, 49, 50, 8, 51, 52, 53, 54, 55, 9, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 0, };
-    uint8_t best_mapping[] = { 9, 18, 25, 27, 55, 66, LAST_MATRIX };
+    // 25 pacman, 57 BM gif
+    uint8_t demo_mapping[] = { 57, LAST_MATRIX, 2, 23, 1, 12, 18, 19, 20, 21, 22, 24, 3, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 7, 46, 47, 48, 49, 50, 8, 51, 52, 53, 54, 55, 9, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 0, };
+    uint8_t best_mapping[] = { 9, 25, 27, 55, 66, LAST_MATRIX };
 
     #define RECV_PIN 34
-#else // not ESP32 (ESP8266)
+#elif mheight == 32
     #define LAST_MATRIX 69
     // Which demos are shown, and in which order
     // skip 8 burn, 9 safety third
-    uint8_t demo_mapping[] = { LAST_MATRIX, 2, 23, 1, 4, 18, 19, 20, 21, 22, 24, 25, 3, 26, 27, 28, 29, 30, 5, 31, 32, 33, 34, 35, 6, 36, 37, 38, 39, 7, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 0, };
+    uint8_t demo_mapping[] = { LAST_MATRIX, 2, 23, 1, 4, 12, 18, 19, 20, 21, 22, 24, 25, 3, 26, 27, 28, 29, 30, 5, 31, 32, 33, 34, 35, 6, 36, 37, 38, 39, 7, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 0, };
     // On ESP8266, I drive 3x 32x8 panels plus neopixel strips
     // 55 hypno, 60 dance girl, 66 sonix
-    uint8_t best_mapping[] = { 5, 18, 24, 25, 27, 55, 60, 66, LAST_MATRIX };
+    uint8_t best_mapping[] = { 5, 12, 24, 25, 27, 55, 60, 66, LAST_MATRIX };
     #define M32B8X3
     #define STRIP_NUM_LEDS 48
 
@@ -33,9 +61,12 @@
     #define NEOPIXEL_PIN D1 // GPIO5
 
     #define RECV_PIN D4
-#endif // ESP32
-#define demo_cnt (sizeof(demo_mapping)/sizeof(demo_mapping[0]))
-#define best_cnt (sizeof(best_mapping)/sizeof(best_mapping[0]))
+#else
+#error "unknown matrix height, no idea what demos to run"
+#endif 
+
+#define demo_cnt ARRAY_SIZE(demo_mapping)
+#define best_cnt ARRAY_SIZE(best_mapping)
 
 // Disable fonts in many sizes
 // Sketch uses 283784 bytes (27%) of program storage space. Maximum is 1044464 bytes.
